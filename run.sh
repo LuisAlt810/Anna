d30d34916d0cc582bda7be950b81a72005b6e6d8
LuisAlt810 <Luisdiko1410@gmail.com>
2025-07-03 15:29:44 +0000

    LK

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
-pm2 start index.js --name "Anna-the-guardian"
+cat > start.sh << 'EOF'
+#!/bin/bash
+
+while true
+do
+  node index.js
+  
+  echo "Bot crashed or exited, restarting in 60 seconds..."
+  sleep 60
+done
+EOF
+
